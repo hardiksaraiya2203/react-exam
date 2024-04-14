641e20ca5f116bfc2e4be4cd4999381ac46c350b
hardiksaraiya2203 <hardiksaraiya2203@gmail.com>
2024-04-14 10:58:00 +0530

    changes

--- src/R.jsx
+++ src/R.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Product from "./Products";
-import Contact from "./Contact"
 import ContactManagement from "./ContactManagement"
+import Contact from "./Contact"
 
 const Routing = () => {
   return (
     <div>
       <Routes>
         <Route path="/products" element={<Product />} />
-        <Route path="/contact" element={<Contact />} />
         <Route path="/ContactManagement" element={<ContactManagement />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
